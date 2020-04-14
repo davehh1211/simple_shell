b9ccd604988655090ad659de979cba534a5a1188
davehh1211 <davehh1211@gmail.com>
2020-04-14 06:43:16 +0000

    add file to find path of commands

---
+++ pathfinder.c
@@ -0,0 +1,101 @@
+#include "shellhead.h"
+/**
+ * pathcommand - function to concatenate the path with the command
+ * @result: command path found
+ * @command: command given by the user
+ * Return: the path found
+ */
+char *pathcommand(char **result, char *command)
+{
+	char *pathcomplete;
+	int ffile, lencom, lenre;
+
+	if (command == NULL || result == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*result != NULL)
+	{
+		lencom = _strlen(command);
+		lenre =  _strlen(*result)
+		pathcomplete = _calloc(lencom + 2 + lenre, sizeof(char));
+		if (pathcomplete == NULL)
+		{
+			perror("Error allocating memory");
+			return (NULL);
+		}
+		_strcat(pathcomplete, *result);
+		_strcat(pathcomplete, "/");
+		_strcat(pathcomplete, command);
+
+		ffile = access(pathcomplete, F_OK);
+		if (ffile != -1)
+		{
+			return (pathcomplete);
+		}
+		free(pathcomplete); /* might be result */
+		result++;
+	}
+	return (NULL);
+}
+/**
+ * count_dir - function that counts the amount of directories.
+ * @str: the full path to be counted.
+ * Return: the amount of directories
+ */
+int count_dir(char *str)
+{
+	int i = 0, words = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+	if (str[i] != ':' && (str[i + 1] == ':' || str[i + 1] == '\0'))
+		words++;
+	}
+	return (words + 1);
+}
+/**
+ * pathgetter - function to extract the full path of environment var
+ * Return: the path found.
+ */
+char **pathgetter(void)
+{
+	char *ptrenv, *tok, **result;
+	unsigned int words, i;
+
+	ptrenv = _getenv("PATH");
+	/*
+	*if (ptrenv == NULL)
+	*return (NULL);
+	*/
+	words = count_dir(ptrenv);
+	result = _calloc(words + 1, sizeof(char *));
+	if (result == NULL)
+	{
+		perror("Error: memory allocation failed");
+		return (NULL);
+	}
+	tok = _strtok(ptrenv, ":"); /* missing a line */
+	*result = tok;
+	for (i = 1; i < words; i++) /*i starts at one */
+	{
+		tok = _strtok(NULL, ":"); /*might be inverted */
+		*(result + i) = tok;
+	}
+	*(result + i) = NULL;
+	return (result);
+}
+/**
+ * pathfinder - function to find the path of environment var
+ * @command: command to be used in the shell
+ * Return: the path found.
+ */
+char *pathfinder(char *command)
+{
+	char **cmdpath = cmdpath = pathgetter(); /*it gets the path*/
+	char *result;
+
+	result = pathcommand(cmdpath, command); /*concat the commmand with the path*/
+	return (result);
+}
