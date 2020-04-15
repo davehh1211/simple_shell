195d0f7e02d88f7f702eca5450a87a131f648e51
julianfrancor <julianfrancor@gmail.com>
2020-04-15 04:28:45 +0000

    fixing bugs in shell main function and adding features to get the signals

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,16 @@
 #include "shellhead.h"
 
 /**
+ * controlc - handle to avoid exitting with ctrl+c
+ * @s: signal to be evaluated.
+ * Return: nothing.
+ */
+void controlc(int s)
+{
+	s *= 1;
+	write(1, "\n$ ", 3);
+}
+/**
  * main - main function of
  * program simple_shell
  * Return: on succes return 0.
@@ -10,21 +20,23 @@ int main(void)
 	char *lineptr = NULL, **args;
 	int status = 0;
 
-	while (status == 0)
-	{
-		/*prints prompt*/
-		write(STDOUT_FILENO, "$ ", 2);
+		do {
+			/*prints prompt*/
+			signal(SIGINT, controlc);
+				write(STDOUT_FILENO, "$ ", 2);
 
-		lineptr = _getline();
-		if (lineptr == 0)
-		{
+			lineptr = _getline();
+			if (lineptr == 0)
+			{
+				free(lineptr);
+				continue;
+			}
+			args = tokenizer(lineptr, TOK_DELIMITER);
+			status = execute(args);
+			free(args);
 			free(lineptr);
-			continue;
-		}
-		args = tokenizer(lineptr, TOK_DELIMITER);
-		status = execute(args);
-		free(args);
-		free(lineptr);
-	}
+			lineptr = NULL;
+		} while (status);
+
 	return (0);
 }
